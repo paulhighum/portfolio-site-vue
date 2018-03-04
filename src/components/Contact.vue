facaf286fc84f99ace0b6cd008d0711b6d7bdaf1
Paul Highum <phigh07@gmail.com>
2018-03-04 15:10:27 -0700

    stats contact form component

---
+++ src/components/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="Contact">
+    <h2>Send me a Message</h2>
+    <form class="" action="index.html" method="post">
+      <label>Name:</label>
+      <input type="text" name="name" >
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "Contact",
+  data() {
+    return {
+      emailObject {
+        name: "",
+        email: "",
+        message: "",
+      }
+    }
+  },
+  methods: {
+    sendEmail() {
+      console.log(this.emailObject, "sent")
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
